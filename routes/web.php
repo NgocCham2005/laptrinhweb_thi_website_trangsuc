73e2831f1443a927ec88cb6ffaee2df722b404db
Thu Trang <saira87205@gmail.com>
2026-05-25 14:19:02 +0700

    Add BannerController import to web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VoucherController;
+use App\Http\Controllers\BannerController;
 
 Route::get('/', function () {
     return view('welcome');
