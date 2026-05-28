9e476115940895c713802e7c6c3988a650198393
vangnguyen2005 <vangthinguyen2005@gmail.com>
2026-05-28 19:03:40 +0700

    Add files via upload

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>Luminous Jewelry</title>
+    <link rel="stylesheet" href="{{ asset('css/form.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+</head>
+<body>
+
+    @yield('content')
+
+</body>
+</html>
