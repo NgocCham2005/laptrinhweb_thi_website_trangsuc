ec049151b1a246383c8bd7df6ead6793af10e452
Vangnguyen <vangthinguyen2005@gmail.com>
2026-06-03 13:55:39 +0700

    Cap nhat app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,16 +4,19 @@
 <head>
     <meta charset="UTF-8">
     <title>Luminous Jewelry</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/component.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/variables.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/form.css') }}">
     <link rel="stylesheet" href="{{ asset('css/user-layout.css') }}">
-   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-   <link rel="stylesheet" href="{{ asset('css/component.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/product.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/product-detail.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css">
     <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
     <script src="{{ asset('js/slider.js') }}"></script>
-    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
 </head>
-    
+
 <body>
 
      @include('partials.header') 
